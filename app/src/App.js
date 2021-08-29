c7631f6cac0f6389bdc4847f89809ee332ecdaa3
AlejandroDR <alejandro@finco.co>
2021-08-29 12:39:07 -0500

    added theme provider to app

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,14 @@
+import { createTheme, ThemeProvider } from "@material-ui/core";
+
+// TODO: add our own styles
+const theme = createTheme({});
+
 function App() {
-  return <div className="App">Pokemon</div>;
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>Pokemon</ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
